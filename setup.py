f32da8b4624b07f4498e861870670c228972d702
Janik Kokot <38459178+janikkokot@users.noreply.github.com>
2022-07-20 20:05:42 +0200

    Add necesarry files to install as package

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.2"
+DESCRIPTION = "GetContacts-analysis"
+LONG_DESCRIPTION = "github.com/liedllab/Getcontacts-analysis"
+
+setup(
+        name="gc_analysis",
+        version=VERSION,
+        author="Janik Kokot",
+        author_email="janik@kokot.cc",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[
+            "matplotlib",
+            "seaborn",
+            "pandas",
+            "numpy",
+            ],
+
+        keywords=["python", "GetContacts", "fingerprint", "flareplot"],
+        classifiers= [
+            "Intended Audience :: scientific",
+            "Programming Language :: Python :: 3",
+            "Operating System :: Linux :: Debian",
+            ],
+        )
